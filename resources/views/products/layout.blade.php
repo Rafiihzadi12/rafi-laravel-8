bb7962c9f908cbacde984f01e4690e10776713df
rafiihzadi <rafiihzadi@gmail.com>
2023-07-14 09:44:27 +0700

    update

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -2,14 +2,14 @@
 <html>
 <head>
     <title>Laravel 8 CRUD Application</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootsrap/4.0.0-alpha/css/bootstrap.css" rel=stylesheet>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Laravel 8 CRUD Application</h1>
-    <div class="container">
+
+<div class="container">
     <br>
     @yield('content')
-    </div>
-    
+</div>
+
 </body>
 </html>
